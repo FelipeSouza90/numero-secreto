1bab1816517ee8662a243833ae5c3fbf480421a3
Felipe Souza <felipe12sp@yahoo.com.br>
2025-03-02 12:39:39 -0300

    Revert "02/03"
    This reverts commit 9dea0e65c8f7c151e7e6cc7a98e41423a0765171.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function exibirNumero(numero) {
-  let numero = ('Digite um numero de 0 a 100');
+  let numero = ('Digite um numero de 0 a 1000');
 }
 
 exibirNumero(`O dobro do número é ${numero*100}`);
