9dea0e65c8f7c151e7e6cc7a98e41423a0765171
Felipe Souza <felipe12sp@yahoo.com.br>
2025-03-02 12:32:32 -0300

    02/03

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function exibirNumero(numero) {
-  let numero = ('Digite um numero de 0 a 1000');
+  let numero = ('Digite um numero de 0 a 100');
 }
 
 exibirNumero(`O dobro do número é ${numero*100}`);
